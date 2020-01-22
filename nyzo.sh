e7895dc93c8ac78bb982b6807abae7e287acc2f9
neicedai <37585056+neicedai@users.noreply.github.com>
2020-01-22 18:57:34 +0800

    Create nyzo.sh

---
+++ nyzo.sh
@@ -0,0 +1,39 @@
+
+cd /home/ &&
+
+mkdir ubuntu &&
+
+cd ubuntu &&
+
+sudo ufw allow 9444/tcp &&
+
+sudo apt update &&
+
+sudo apt install haveged openjdk-8-jdk supervisor -y && 
+sudo git clone https://github.com/n-y-z-o/nyzoVerifier.git &&
+cd nyzoVerifier &&
+
+sudo ./gradlew build &&
+
+sudo mkdir -p /var/lib/nyzo/production &&
+
+sudo cp trusted_entry_points /var/lib/nyzo/production &&
+
+chmod 777 nyzoVerifier.sh &&
+
+./nyzoVerifier.sh &&
+
+sudo cp nyzoVerifier.conf /etc/supervisor/conf.d/ &&
+
+
+sleep 4s &&
+
+echo “@reboot sudo supervisorctl reload” >> mycron &&
+
+sudo crontab mycron &&
+
+rm mycron &&
+
+sudo supervisorctl status &&
+
+more /var/lib/nyzo/production/verifier_private_seed
