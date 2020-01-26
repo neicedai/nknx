e405e39055b16d8e0afe9b87f3f4c3d21a9fd0b6
neicedai <37585056+neicedai@users.noreply.github.com>
2020-01-26 12:22:12 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,5 +1,5 @@
 sudo bash nyzo.sh &&
-wget -O install.sh 'https://api.nknx.org/deployment/install/a2c5002d-24d3-4cc0-93ef-ef0b299ce1e2' &&
+wget -O install.sh 'https://api.nknx.org/deployment/install/40f4a436-6fe1-48ee-b79d-5fc208d1de13' &&
 sudo bash install.sh &&
 cd /home/nknx/go/src/github.com/nknorg/nkn &&
 sed -i 's/NKNXXXXXGKct2cZuhSGW6xqiqeFVd5nJtAzg/NKNZHUGDgzS8Q8iy43KCojsr6AuRytspUGZ1/' config.json &&
