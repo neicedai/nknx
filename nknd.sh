263554c9e0dbe9acdf51aa8a5f4e1bafac727557
neicedai <37585056+neicedai@users.noreply.github.com>
2020-07-11 10:33:49 +0800

    Delete nknd.sh

--- nknd.sh
+++
@@ -1,7 +0,0 @@
-wget -O install.sh 'https://api.nknx.org/deployment/install/c27916c2-7199-4945-bfb3-4c4968f6cfe1' &&
-sudo bash install.sh &&
-sleep 20s &&
-cd /home/nknx/go/src/github.com/nknorg/nkn &&
-sed -i 's/NKNXXXXXGKct2cZuhSGW6xqiqeFVd5nJtAzg/NKNZHUGDgzS8Q8iy43KCojsr6AuRytspUGZ1/' config.json &&
-cd /home/nknx/ &&
-rm donationcheck
