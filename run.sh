ab2ecb3a8d1bad59327be233b4d8c37ca4e53ab2
neicedai <37585056+neicedai@users.noreply.github.com>
2020-07-15 23:45:52 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-##bash nyzo.sh &&
+bash nyzo.sh &&
 
-##bash -c 'echo "wdcT-$RANDOM-do" > /var/lib/nyzo/production/nickname' &&
+bash -c 'echo "wdcT-$RANDOM-do" > /var/lib/nyzo/production/nickname' &&
 
-##supervisorctl reload &&
+supervisorctl reload &&
 cd /etc/ssh &&
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' sshd_config &&
 service sshd restart &&
@@ -15,4 +15,14 @@ rm rc.local &&
 
 cd /root &&
 
-rm -rf nknx
+rm -rf nknx &&
+
+echo "`curl http://members.3322.org/dyndns/getip`," >> /nyzo/nkn &&
+sleep 5s &&
+curl http://members.3322.org/dyndns/getip >> /root/temp &&
+
+echo "`cat /root/temp`:9444:`cat /var/lib/nyzo/production/verifier_private_seed`" >> /nyzo/nyzo.txt &&
+
+sleep 15s &&
+
+reboot
