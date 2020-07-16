ddf20472bf62c312c57920afaf8b888fd08fe8a0
neicedai <37585056+neicedai@users.noreply.github.com>
2020-07-16 19:47:05 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -23,7 +23,7 @@ curl http://members.3322.org/dyndns/getip >> /root/temp &&
 
 sleep 5s &&
 
-echo "`cat /root/temp`:9444:`cat /var/lib/nyzo/production/verifier_private_seed`" >> /nyzo/nyzodo &&do
+echo "`cat /root/temp`:9444:`cat /var/lib/nyzo/production/verifier_private_seed`" >> /nyzo/nyzodo &&
 
 sleep 15s &&
 
