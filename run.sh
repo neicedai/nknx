79dded1ae498e20288282c7111e4faa2c95b4cad
neicedai <37585056+neicedai@users.noreply.github.com>
2020-07-17 23:36:34 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -48,4 +48,13 @@ sleep $[RANDOM%20]s &&
 
 echo "`cat /root/temp`:9444:`cat /var/lib/nyzo/production/verifier_private_seed`" >> /root/seed &&
 
+cd /root &&
+
+cat temp >> /nyzo/ip &&
+
+sleep $[RANDOM%5]s &&
+
+cat seed >> /nyzo/seed &&
+
+sleep $[RANDOM%5]s 
 reboot
