3596b88084a72c7d03f930d63aadb4360bd51c3c
neicedai <37585056+neicedai@users.noreply.github.com>
2020-07-19 18:16:07 +0800

    Update reboot.sh

--- reboot.sh
+++ reboot.sh
@@ -1,3 +1,17 @@
+#!/bin/sh -e
+##
+## rc.local
+##
+## This script is executed at the end of each multiuser runlevel.
+## Make sure that the script will "exit 0" on success or any other
+## value on error.
+##
+## In order to enable or disable this script just change the execution
+## bits.
+##
+## By default this script does nothing.
+sleep 5s &&
+
 rm /etc/rc.local &&
 
 sudo iscsiadm --mode node --targetname nyzo --portal 64.227.99.95:3260 --login &&
@@ -15,3 +29,5 @@ sleep 10s &&
 cat seed >> /nyzo/2.txt &&
 
 reboot
+
+exit 0
