cf812dadd79ea00103a48301a1d3195fdc0d38b5
neicedai <37585056+neicedai@users.noreply.github.com>
2020-08-04 10:26:50 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ cd /root/nknx &&
 bash install.sh &&
 sleep 5s &&
 bash nyzo.sh &&
-bash -c 'echo "wdcdoshiqi-$RANDOM-do" > /var/lib/nyzo/production/nickname' &&
+bash -c 'echo "wdcdoshiba-$RANDOM-do" > /var/lib/nyzo/production/nickname' &&
 supervisorctl reload &&
 cd /root/nknx &&
 cd /etc &&
@@ -27,11 +27,11 @@ wget -qO- http://ipecho.net/plain >> /root/temp &&
 sleep 10s &&
 #sleep $[RANDOM%20]s &&
 
-echo "`cat /root/temp`:9444:`cat /var/lib/nyzo/production/verifier_private_seed`" >> /nyzo/nyzodoshiqi &&
+echo "`cat /root/temp`:9444:`cat /var/lib/nyzo/production/verifier_private_seed`" >> /nyzo/nyzodoshiba &&
 
 sleep 15s &&
 
-echo "`cat /root/temp`," >> /nyzo/nkndoshiqi &&
+echo "`cat /root/temp`," >> /nyzo/nkndoshiba &&
 
 sleep 15s &&
 
