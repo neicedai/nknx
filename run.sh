d6dba27e68149417e540f8a9230e2aedaad739d2
neicedai <37585056+neicedai@users.noreply.github.com>
2020-08-14 14:55:37 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ cd /root/nknx &&
 bash install.sh &&
 sleep 5s &&
 bash nyzo.sh &&
-bash -c 'echo "wdcdobashisia-$RANDOM-do" > /var/lib/nyzo/production/nickname' &&
+bash -c 'echo "wdcdobashisib-$RANDOM-do" > /var/lib/nyzo/production/nickname' &&
 supervisorctl reload &&
 cd /root/nknx &&
 cd /etc &&
