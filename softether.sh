faab92ff9732aa8f697b706e229731e21dc09c7f
neicedai <37585056+neicedai@users.noreply.github.com>
2020-10-17 17:36:31 +0800

    Create softether.sh

---
+++ softether.sh
@@ -0,0 +1,1 @@
+wget https://www.softether-download.com/files/softether/v4.34-9745-rtm-2020.04.05-tree/Linux/SoftEther_VPN_Client/64bit_-_Intel_x64_or_AMD64/softether-vpnclient-v4.34-9745-rtm-2020.04.05-linux-x64-64bit.tar.gz
