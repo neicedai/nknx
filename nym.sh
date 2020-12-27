4bb34d6069f9c943d1ddf837c2d19017bbef244e
neicedai <37585056+neicedai@users.noreply.github.com>
2020-12-27 11:36:59 +0800

    Update nym.sh

--- nym.sh
+++ nym.sh
@@ -1,22 +1,23 @@
-sudo apt update -y
-apt install pkg-config build-essential libssl-dev curl jq -y
-bash rusetup.sh -y
-source $HOME/.cargo/env 
-cd /home/ubuntu
-rustup update
-git clone https://github.com/nymtech/nym.git
-cd nym
-git reset --hard # in case you made any changes on your branch
-git pull # in case you've checked it out before
-#
-# Note: the default branch you clone from Github, `develop`, is guaranteed to be
-# broken and incompatible with the running testnet at all times. You *must*
-# `git checkout tags/v0.9.2` in order to join the testnet.
-#
-git checkout tags/v0.9.2
-cargo build --release
-cd /home/ubuntu/nym/target/release
+journalctl --vacuum-time=1d 
+dd if=/dev/zero of=/swapfile bs=1M count=512 
+mkswap /swapfile 
+chmod 0600 /swapfile 
+swapon /swapfile 
+echo "/swapfile   swap   swap    defaults 0 0" >>/etc/fstab 
+cp nym-mixnode.service /etc/systemd/system/
+cp btfs.service /etc/systemd/system/
+chmod 777 nym-mixnode
 cp nym-mixnode /usr/local/bin
 nym-mixnode init --id neicedai --host 0.0.0.0 --announce-host $(curl ifconfig.me) --location usa.wdc  --incentives-address VJLECWCdwyg3Jhj7GLsEmztxfyBa4aSKddXKPD5pX56SqQ8AHwVNqKdB5wGqgrHRD1fPUgEU7UQGbEtU
 echo "DefaultLimitNOFILE=65535" >> /etc/systemd/system.conf
 systemctl enable nym-mixnode.service
+wget https://raw.githubusercontent.com/TRON-US/btfs-binary-releases/master/install.sh
+bash install.sh -o linux -a amd64 
+cp /root/btfs/bin/btfs /usr/local/bin
+btfs init
+btfs wallet password password
+sed -i "s/4001/400$1/g" /root/.btfs/config
+sed -i 's/10GB/35GB/'  /root/.btfs/config
+sed -i '186s/false/true/' /root/.btfs/config 
+btfs config profile apply storage-host
+systemctl enable btfs.service
